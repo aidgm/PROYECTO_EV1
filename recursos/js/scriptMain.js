a8cbf7c6ae3b69d4fcc33118d972d5bb9488e339
Aida <aidagm026@gmail.com>
2024-12-11 12:10:51 +0100

    4 commit: añadir css y js para main

---
+++ recursos/js/scriptMain.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    const contenedorProducto = document.getElementById("contenedor-productos");
+    const botonesCategorias = document.querySelectorAll(".boton-categoria");
+    
+})
