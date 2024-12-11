d6d183b15ec697494e7903432b701eda2f667325
Aida <aidagm026@gmail.com>
2024-12-11 14:32:48 +0100

    5 commit: css de main

--- recursos/js/scriptMain.js
+++ recursos/js/scriptMain.js
@@ -1,5 +1,3 @@
-document.addEventListener("DOMContentLoaded",() =>{
-    const contenedorProducto = document.getElementById("contenedor-productos");
-    const botonesCategorias = document.querySelectorAll(".boton-categoria");
-    
-})
+let productos = [];
+let carrito = [];
+
